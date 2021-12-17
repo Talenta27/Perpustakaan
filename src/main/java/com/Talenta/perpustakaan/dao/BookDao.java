97769834a51afcd5c2d857ebb258cf5933b41cb3
Talenta27 <90179830+Talenta27@users.noreply.github.com>
2021-12-17 09:34:19 +0700

    Add files via upload

---
+++ src/main/java/com/Talenta/perpustakaan/dao/BookDao.java
@@ -0,0 +1,33 @@
+package com.Talenta.perpustakaan.dao;
+
+import com.Talenta.perpustakaan.entity.Book;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Predicate;
+import java.util.List;
+
+@Repository
+
+
+public class BookDao extends BaseDao<Book> {
+
+    @Override
+    public List<Predicate> predicates(Book param, CriteriaBuilder builder, Root<Book> root, boolean isCount) {
+        List<Predicate> predicates = super.predicates(param, builder, root, isCount);
+
+        if (param != null){
+            if (param.getTitle() != null){
+                predicates.add(builder.like(root.get("title"), "%" + param.getTitle() + "%"));
+
+            }
+            if (param.getPublisher() != null){
+                predicates.add(builder.like(root.get("publisher"), "%" + param.getPublisher() + "%"));
+            }
+        }
+
+        return predicates;
+    }
+
+}
